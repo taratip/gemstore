78b9088c184fe5bffaee0ae70190a6c62f681767
Taratip Jaikaeo <tjaikaeo@gmail.com>
2018-10-03 11:49:13 -0400

    View structure created, TODO: view functionality

---
+++ src/components/views/checkout/checkout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './checkout.css';
+
+class Checkout extends Component {
+  render() {
+    return (
+      <div className="Checkout">
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default Checkout;
---
+++ src/components/views/home/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default Home;
