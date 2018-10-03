32d436567d6b6e06821651ccd44f5c2ba9e56aa1
Taratip Jaikaeo <tjaikaeo@gmail.com>
2018-10-03 10:59:42 -0400

    Included State for App component, setup products file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import products from './static/data/products.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      total: undefined,
+      cart: undefined,
+      products: undefined
+    }
+  }
+
+  componentWillMount() {
+    this.setState({
+      total: 0,
+      cart: [],
+      products: products
+    });
+  }
+
   render() {
     return (
       <div className="App">
