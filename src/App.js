f5aeb0a067fe1e43821954ba0bed968d33cf3559
Taratip Jaikaeo <tjaikaeo@gmail.com>
2018-10-04 14:03:32 -0400

    Checkout page functioning properly, TODO: Add firebase cloud storage

--- src/App.js
+++ src/App.js
@@ -59,13 +59,27 @@ class App extends Component {
     this.calcTotal();
   }
 
+  removeItem = (id) => {
+    let items = this.state.cart;
+
+    for (let i = 0;i < items.length; i++) {
+      if (items[i].id === id) {
+        items.splice(i, 1);
+        break;
+      }
+    }
+
+    this.setState({ cart: items });
+    this.calcTotal();
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar total={this.state.total} />
         <Switch>
           <Route exact path='/' render={() => <Home addItem={this.addItem} products={this.state.products} />}></Route>
-          <Route excat path='/checkout' render={() => <Checkout total={this.state.total} />}></Route>
+          <Route excat path='/checkout' render={() => <Checkout cart={this.state.cart} total={this.state.total} removeItem={this.removeItem} />}></Route>
         </Switch>
       </div>
     );
